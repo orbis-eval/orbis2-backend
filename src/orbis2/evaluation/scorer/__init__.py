4eed8a4c48edacbbc4485f47a8b1588d75e7e795
Albert Weichselbraun <albert@weichselbraun.net>
2022-11-07 07:49:08 +0100

    wip: improved scoring.

--- src/orbis2/evaluation/scorer/__init__.py
+++ src/orbis2/evaluation/scorer/__init__.py
@@ -23,16 +23,21 @@ class Scorer:
 
     def score_annotation_list(self, true_annotations: List[Annotation],
                               predicted_annotations: List[Annotation]) -> List:
-        # TODO: correctly align annotations prior to scoring (!)
-        #       and ensure that we do not score annotations multiple times.
         tp = []
         fp = []
         fn = []
         true_annotations.sort()
         predicted_annotations.sort()
         current_pred_idx = 0
+
+        # redo logic:
+        #  1. create sets of overlapping annotations => match candidates
+        #  2. determine sets of matching annotations => improved match cand.
+        #  3. remove duplicates, ensuring that we maximize matches
+        #  4. move all other annotations either into the fp or fn category (!)
+
         for true in true_annotations:
-            # add all annotations that are aligned for the current 'true'
+            # add all annotations located before for the current 'true'
             # annotation to the list of false positives (fp)
             while current_pred_idx < len(predicted_annotations):
                 if not predicted_annotations[current_pred_idx].end < \
@@ -42,6 +47,11 @@ class Scorer:
                     fp.append(predicted_annotations[current_pred_idx])
                     current_pred_idx += 1
 
+            # assign all overlapping annotations to either tp or fp
+            # warning -> this might be too simplistic, since the overlap
+            # might be a working match with a later true annotation
+            # --
+            # alternative: create sets of
             while current_pred_idx < len(predicted_annotations) and  \
                     overlap(true, predicted_annotations[current_pred_idx]):
                 pred = predicted_annotations[current_pred_idx]
@@ -50,10 +60,12 @@ class Scorer:
                         self.surface_matcher.score_annotation(true, pred),
                         self.entity_scorer.score_annotation(true, pred)) > 0:
                     tp.append((pred, true))
-                    # TODO: continue with true_annotations / break & continue?
+                    true = None
+                    break
                 else:
                     fp.append(pred)
 
-            fn.append(true)
+            if true:
+                fn.append(true)
 
         return tp, fp, fn
