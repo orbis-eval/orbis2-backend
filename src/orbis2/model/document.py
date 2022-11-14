a954e8f1c682b094b8b1cef2c68856d82771633e
Andreas Fraefel <andreas.fraefel@fhgr.ch>
2022-11-14 09:26:21 +0100

    add key to document

--- src/orbis2/model/document.py
+++ src/orbis2/model/document.py
@@ -5,23 +5,26 @@ from orbis2.model.metadata import Metadata
 
 class Document:
 
-    def __init__(self, content: str, run_id: int = None, metadata: [Metadata] = None, done: bool = False):
+    def __init__(self, content: str, key: str = '', run_id: int = None, metadata: [Metadata] = None,
+                 done: bool = False):
         """
         CONSTRUCTOR
 
         """
         self.document_id = None
         self.content = content
+        self.key = key
         self.run_id = run_id
         self.metadata = metadata if metadata else []
         self.done = done
 
     @classmethod
     def from_document_dao(cls, document_dao: DocumentDao, run_id: int, done: bool) -> 'Document':
-        document = cls(document_dao.content, run_id, Metadata.from_metadata_daos(document_dao.meta_data), done)
+        document = cls(document_dao.content, document_dao.key, run_id,
+                       Metadata.from_metadata_daos(document_dao.meta_data), done)
         document.document_id = document_dao.document_id
         return document
 
     def to_dao(self) -> DocumentDao:
-        return DocumentDao(document_id=self.document_id, content=self.content,
+        return DocumentDao(document_id=self.document_id, content=self.content, key=self.key,
                            meta_data=Metadata.to_metadata_daos(self.metadata))
