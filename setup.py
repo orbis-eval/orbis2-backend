b3f79747c84d11f3c3942a6a6c536221e9f46c1a
Andreas Fraefel <andreas.fraefel@fhgr.ch>
2022-12-05 11:39:42 +0100

    initial CI/CD

---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+"""orbis2 backend setup script."""
+
+import sys
+from pathlib import Path
+from setuptools import setup, find_packages
+from os import path
+
+here = Path(path.dirname(__file__)).resolve()
+sys.path.insert(0, path.join(here, 'src'))
+
+from src.orbis2.metadata import __version__
+
+setup(
+    # Metadata
+    name='orbis2-backend',
+    version=__version__,
+    description='orbis2-backend - backend for the orbis2 framework, used for annotating and evaluating nlp documents',
+    python_requires='>=3.9',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: Markup :: HTML',
+        'Topic :: Utilities',
+        'Programming Language :: Python :: 3.9',
+    ],
+    package_dir={'': 'src'},
+
+    # Package List
+    packages=find_packages('src'),
+
+    # Requirements
+    install_requires=[
+        'SQLAlchemy',
+        'SQLAlchemy-Utils',
+        'psycopg2-binary',
+        'fastapi',
+        'pytest',
+        'pydantic',
+        'xxhash',
+        'scikit-learn',
+        'uvicorn'
+    ],
+)
