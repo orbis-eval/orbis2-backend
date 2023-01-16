796c45ed79e0101a891ba0e908274dc73cdda970
Albert Weichselbraun <albert@weichselbraun.net>
2023-01-16 13:52:40 +0100

    wip: streamlit app.

--- scripts/visualize.py
+++ scripts/visualize.py
@@ -10,13 +10,11 @@ from pickle import load
 
 
 def annnotate_document(document, document_annotations):
-    annotations = document_annotations[document]
-
     text = document.content.replace("\n", " ")
     content_idx = 0
 
     annotated_content = []
-    for annotation in sorted(annotations, key=lambda x: x.start_indices[0]):
+    for annotation in sorted(document_annotations[document], key=lambda x: x.start_indices[0]):
         prefix = text[content_idx:annotation.start_indices[0]]
 
         annotated_content.append(prefix)
@@ -27,24 +25,22 @@ def annnotate_document(document, document_annotations):
     return annotated_content
 
 
-
-
 st.write("## Orbis Evaluation")
 
 obj = load(open('../test.dump', 'rb'))
 ref_documents = list(obj['reference'].keys())
 
-selected = st.number_input('Pick a document', 0, len(ref_documents)-1)
+filter_text = ''
+documents = [r for r in ref_documents if not filter_text or filter_text in r.content]
+selected = st.number_input('Pick a document', 0, len(documents)-1)
 
 reference, annotator = st.columns(2)
-
-
 with reference:
     st.header("Reference")
-    annotated = annnotate_document(ref_documents[selected], obj['reference'])
+    annotated = annnotate_document(documents[selected], obj['reference'])
     annotated_text(*annotated)
 
 with annotator:
     st.header("Annotator")
-    annotated = annnotate_document(ref_documents[selected], obj['annotator'])
+    annotated = annnotate_document(documents[selected], obj['annotator'])
     annotated_text(*annotated)
