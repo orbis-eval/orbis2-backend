b8069b8ca436aa6c690e8fdefb60d53472336dcf
André Glatzl <andre.glatzl@fhgr.ch>
2024-03-18 10:30:01 +0100

    Fix single-file issue on createRun

--- src/orbis2/corpus_import/format/tools/helper_importer.py
+++ src/orbis2/corpus_import/format/tools/helper_importer.py
@@ -1,15 +1,14 @@
-from typing import List
 from .labelstudio_importer import LabelStudioImporter
 from .doccano_importer import DoccanoImporter
 
 
 class HelperImporter:
     @staticmethod
-    def get_annotated_documents_and_types(files: List[dict] = None) -> tuple:
+    def get_annotated_documents_and_types(file: dict = None) -> tuple:
         documents_with_annotations_list = []
         annotation_types = []
 
-        for file in files:
+        if file:
             if file["filename"].endswith(".json"):
                 documents_with_annotations_list.extend(LabelStudioImporter.get_annotated_documents(file))
                 annotation_types = list(set(annotation_types + LabelStudioImporter.get_annotation_types(file)))
