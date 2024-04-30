285a0090b0afa952b2c40f5c9a4f993cc7696966
André Glatzl <andre.glatzl@fhgr.ch>
2024-04-30 17:03:37 +0200

    fix(orbis2): lint code

--- src/orbis2/model/gold_standard.py
+++ src/orbis2/model/gold_standard.py
@@ -1,16 +1,7 @@
-from typing import Dict, List, Optional
-import datetime
-
 from pydantic import Field
-from xxhash import xxh32_intdigest
 
-from orbis2.database.orbis.entities.run_dao import RunDao
 from orbis2.model.run import Run
-from orbis2.model.annotation import Annotation
-from orbis2.model.base_model import OrbisPydanticBaseModel
-from orbis2.model.corpus import Corpus
-from orbis2.model.document import Document
-from orbis2.evaluation.output_formatter.inter_rater_agreement_result import InterRaterAgreementResult
+
 
 class GoldStandard(Run):
     number_of_runs: int = Field(0, alias="numberOfRuns")
@@ -22,4 +13,3 @@ class GoldStandard(Run):
         super().__init__(**kwargs)
         self.number_of_runs = number_of_runs
         self.number_of_documents = number_of_documents
-
