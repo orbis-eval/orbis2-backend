aeb619ec1b97f50ce31fd9f8c1822e240af39b4e
Andreas Murk <andreas.murk@fhgr.ch>
2024-06-18 09:25:03 +0200

    fix(backend): remove caching
    For the methods get_runs, get_runs_by_name as well as get_next and get_previous document caching does not work as it results in caching errors.

--- src/orbis2/business_logic/orbis_service.py
+++ src/orbis2/business_logic/orbis_service.py
@@ -150,7 +150,6 @@ class OrbisService:
             )
         return document
 
-    @cached(cache)
     def get_next_document(self, run_id: int, document_id: int) -> Optional[Document]:
         """
         Returns:
@@ -163,7 +162,6 @@ class OrbisService:
             doc_obj.run_id = run_id
         return doc_obj
 
-    @cached(cache)
     def get_previous_document(self, run_id: int, document_id: int) -> Optional[Document]:
         """
         Returns:
